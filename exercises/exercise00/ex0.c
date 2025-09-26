2272281754981caf5d97c0f0a3e4a54117603c22
user39261580 <user39261580@gmail.com>
2025-09-26 01:08:03 -0700

    Add input checking for ex0

--- exercises/exercise00/ex0.c
+++ exercises/exercise00/ex0.c
@@ -8,21 +8,26 @@
 #include <string.h>
 
 // Function declaration for recursive Nilakantha series 
-double Nilakantha(int n);
+double nilakantha(int n);
+int is_input_valid(int argc, char const *argv[]);
 
 int main(int argc, char const *argv[])
 {
+  if (!is_input_valid(argc, argv)) {
+    return EXIT_FAILURE;
+  }
+  
   int n = strtol(argv[1], NULL, 10);
   double pi_estimate = 0.0;
   for (int i = 0; i <= n; i++) {
-    pi_estimate += Nilakantha(i);
+    pi_estimate += nilakantha(i);
   }
   printf("Our estimate of Pi is %.20f\n", pi_estimate);
   return EXIT_SUCCESS;
 }
 
 
-double Nilakantha(int n) {
+double nilakantha(int n) {
   if (n == 0) {
     return 3.0;
   } else {
@@ -33,4 +38,23 @@ double Nilakantha(int n) {
       return 0 + term;
     }
   }
+}
+
+// Specific input check function
+int is_input_valid(int argc, char const *argv[]) {
+  // Check whether args count is correct
+  if (argc != 2) {
+    printf("Invalid input\n");
+    return 0;
+  }
+
+  // Check whether the input is a non-negative integer
+  char *endptr;
+  long val = strtol(argv[1], &endptr, 10);
+  if (*endptr != '\0' || val < 0) {
+    printf("Invalid input\n");
+    return 0;
+  }
+
+  return 1;
 }
