4a655f1915c30e581a388a64e32f97da9abb9ef4
user39261580 <user39261580@gmail.com>
2025-09-29 03:46:05 -0700

    Complete function of CopyAndSort and InsertionSort Passed cpplint and valgrind

--- exercises/exercise01/ex1.c
+++ exercises/exercise01/ex1.c
@@ -7,72 +7,75 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
-#include <inttypes.h>
 
-#define SIZE 12
+#define ARRAY_SIZE 12
+
 // Function declarations
-int CopyAndSort(int64_t *input_array, int64_t *output_array, size_t array_length);
-// definition placed before use to avoid implicit declaration issues
-int IsInputValid(int64_t *input_array, int64_t *output_array, size_t array_length) {
-  // Checking for same input and output arrays
-  if (input_array == output_array) {
-    printf("Input and output arrays must be different\n");
-    return 0;
-  }
+int CopyAndSort(int64_t src_array[], int64_t dest_array[], int array_size);
+int IsInputValid(int64_t src_array[], int64_t dest_array[], int array_size);
+void InsertionSort(int64_t new_num, int64_t dest_array[], int array_size);
 
-  // Checking for NULL pointers
-  if (input_array == NULL || output_array == NULL) {
-  for (size_t i = 0; i < SIZE; i++) {
-    printf("%" PRId64 " ", sorted_array[i]);
-  }
-  printf("\n");
+int main(int argc, char const *argv[]) {
+  int64_t unsorted_array[] = {3, 2, -5, 7, 17, 42, 6, 333, 7, 8, -8, 6};
+  int64_t sorted_array[ARRAY_SIZE];
 
-  // (size_t can't be negative, so no negative length check needed)
-  return 1;
-int CopyAndSort(int64_t *input_array, int64_t *output_array, size_t array_length) {
-  if (!IsInputValid(input_array, output_array, array_length)) {
+  if (CopyAndSort(unsorted_array, sorted_array, ARRAY_SIZE) == EXIT_FAILURE) {
     return EXIT_FAILURE;
   }
 
-  if (array_length == 0) {
-    return EXIT_SUCCESS;
-  }
-
-  // Copy input to output
-  for (size_t i = 0; i < array_length; i++) {
-    output_array[i] = input_array[i];
-  }
-
-  // Insertion sort (ascending)
-  for (size_t i = 1; i < array_length; i++) {
-    int64_t key = output_array[i];
-    size_t j = i;
-// (IsInputValid definition moved above) int64_t largest_value = input_array[0];
-
-  for (size_t i = 0; i < array_length; i++) {
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    printf("%ld ", sorted_array[i]);
   }
 
   return EXIT_SUCCESS;
 }
 
-int IsInputValid(int64_t *input_array, int64_t *output_array, size_t array_length) {
+int CopyAndSort(int64_t src_array[], int64_t dest_array[], int array_size) {
+  if (!IsInputValid(src_array, dest_array, array_size)) {
+    return EXIT_FAILURE;
+  }
+
+  // Handle empty array
+  if (array_size == 0) {
+    return EXIT_SUCCESS;
+  }
+
+  dest_array[0] = src_array[0];
+
+  for (int i = 1; i < array_size; i++) {
+    InsertionSort(src_array[i], dest_array, i + 1);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int IsInputValid(int64_t src_array[], int64_t dest_array[], int array_size) {
   // Checking for same input and output arrays
-  if (input_array == output_array) {
+  if (src_array == dest_array) {
     printf("Input and output arrays must be different\n");
     return 0;
-  } 
+  }
 
   // Checking for NULL pointers
-  if (input_array == NULL || output_array == NULL) {
+  if (src_array == NULL || dest_array == NULL) {
     printf("Invalid array pointer\n");
     return 0;
   }
 
-  // Checking for negative array length
-  if (array_length < 0) {
-    printf("Invalid array length\n");
+  // Checking for negative array size
+  if (array_size < 0) {
+    printf("Invalid array size\n");
     return 0;
   }
 
   return 1;
+}
+
+void InsertionSort(int64_t new_num, int64_t dest_array[], int array_size) {
+  int index = array_size - 1;
+  while (index > 0 && dest_array[index - 1] > new_num) {
+    dest_array[index] = dest_array[index - 1];
+    index--;
+  }
+  dest_array[index] = new_num;
 }
