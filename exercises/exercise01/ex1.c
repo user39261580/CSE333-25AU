19a8179dca01cee874df7fe35a9f2051a828ecd0
user39261580 <user39261580@gmail.com>
2025-09-28 23:49:39 -0700

    Add initial implementation of insertion sort in ex1.c

---
+++ exercises/exercise01/ex1.c
@@ -0,0 +1,12 @@
+// Copyright 2025 Tsung-Wei Chin
+// Name: Miles TsungWei Chin
+// Email: twchin@uw.edu
+// Subject: Insertion sort for an array of integers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, char const *argv[]) {
+  return EXIT_SUCCESS;
+}
