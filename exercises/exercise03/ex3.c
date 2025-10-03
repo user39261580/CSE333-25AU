ca0c99119330f850db04bfcc0d3bfa429f1bbbc1
user39261580 <user39261580@gmail.com>
2025-10-03 01:52:54 -0700

    Implement Point3d struct and memory allocation in ex3.c

--- exercises/exercise03/ex3.c
+++ exercises/exercise03/ex3.c
@@ -8,8 +8,51 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+typedef struct Point3d_st{
+  uint16_t x;
+  uint16_t y;
+  uint16_t z;
+} Point3d;
+
+Point3d* AllocatePoint3d(uint16_t x, uint16_t y, uint16_t z);
+
 int main(int argc, char const *argv[])
 {
-  /* code */
-  return 0;
+  uint16_t x = 1;
+  uint16_t y = 2;
+  uint16_t z = 3;
+
+  Point3d* point = AllocatePoint3d(x, y, z);
+
+  if (point == NULL) {
+    printf("Memory allocation failed\n");
+    return EXIT_FAILURE;
+  }
+
+  if (point->x != x) {
+    printf("Point3d.x value is incorrect\n");
+    return EXIT_FAILURE;
+  }
+
+  if (point->y != y) {
+    printf("Point3d.y value is incorrect\n");
+    return EXIT_FAILURE;
+  }
+
+  if (point->z != z) {
+    printf("Point3d.z value is incorrect\n");
+    return EXIT_FAILURE;
+  }
+
+  free(point);
+  return EXIT_SUCCESS;
+}
+
+Point3d* AllocatePoint3d(uint16_t x, uint16_t y, uint16_t z) {
+  Point3d* point = (Point3d*) malloc(sizeof(Point3d));
+
+  point->x = x;
+  point->y = y;
+  point->z = z;
+  return point;
 }
