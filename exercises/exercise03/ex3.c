138fb7e53743b9907f55d9fe9a3f4521bfba5c93
user39261580 <user39261580@gmail.com>
2025-10-03 01:14:23 -0700

    Add initial files of exercise03

---
+++ exercises/exercise03/ex3.c
@@ -0,0 +1,15 @@
+// Copyright 2025 Tsung-Wei Chin
+// Name: Miles TsungWei Chin
+// Email: twchin@uw.edu
+// Goals: Write a program that uses structs, typedef, and dynamic memory allocation (malloc/free).
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
